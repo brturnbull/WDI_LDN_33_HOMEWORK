11cc1c1822a0c91c0fc9dd6526808c8da9d1e802
Bridget <brturnbul@gmail.com>
2018-03-19 21:45:42 +0000

    final working of calculator however stil stuck on square root etc

---
+++ brturnbull/w02d01/main.js
@@ -0,0 +1,23 @@
+var userPrompt = prompt('Do you love maths?!');
+
+  while (userPrompt === 'yes') {
+
+
+    var num1 = parseFloat(prompt('Enter a number'));
+    var operator = prompt('Enter an operator to +, -, /, ^ and *');
+    var num2 = parseFloat(prompt('Enter a number'));
+
+    if (operator === '+') {
+      alert(num1 + num2);
+    } else if (operator === '-'){
+      alert(num1 - num2);
+    } else if (operator === '/'){
+      alert(num1 / num2);
+    } else if (operator === '*'){
+      alert(num1 * num2);
+    } else if (operator === '^'){
+      alert(Math.pow(num1, num2));
+    } else {
+      alert('Please submit a valid equation.');
+    }
+  } prompt;
