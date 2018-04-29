27d7953cdceb8eeb0a1cbe81fe55a25d5e2ffa1a
Bridget <brturnbul@gmail.com>
2018-04-29 18:07:02 +0100

    hw

---
+++ brturnbull/w07d05/citymapper/src/app.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import MainCtrl from './controllers/main';
+
+angular.module('aldgateGrind', [])
+  .controller('MainCtrl', MainCtrl);
---
+++ brturnbull/w07d05/citymapper/src/controllers/main.js
@@ -0,0 +1,5 @@
+function MainCtrl() {
+
+}
+
+export default MainCtrl;
